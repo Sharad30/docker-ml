7c0e1cc6f7019cf88e851291174df8bf4f40ef7d
Sharad Shriyan <sharad30.ss@gmail.com>
2022-08-06 19:23:44 +0530

    feat: adds TabularDataset class which loads and preprocesses the data

---
+++ ml/tabular_dataset.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import List, Optional, Union
+
+import pandas as pd
+from pydantic import BaseModel
+from sklearn.preprocessing import LabelEncoder
+
+
+class TabularDataset(BaseModel):
+    root: Union[str, Path]
+    train: Optional[pd.DataFrame]
+    test: Optional[pd.DataFrame]
+    features: Optional[List[str]]
+    target: Optional[pd.Series]
+    target_str: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def load_df(self):
+        self.root = Path(self.root)
+        self.train = pd.read_csv(self.root / "train.csv")
+        self.test = pd.read_csv(self.root / "test.csv")
+
+        self.features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+        self.target = self.train[self.target_str].copy()
+        return self
+
+    def preprocess(self):
+        self.train[self.features] = self.train[self.features].fillna(self.train[self.features].mean())
+        self.test[self.features] = self.test[self.features].fillna(self.test[self.features].mean())
+
+        le = LabelEncoder()
+        self.train[self.features] = le.fit_transform(self.train[self.features])
+        self.test[self.features] = le.transform(self.test[self.features])
+        return self
